2451d38b38dff61cdb9ec2bbabcf7c594dc37a11
Mark Wolfe <mark@wolfe.id.au>
2011-01-27 23:35:12 +1100

    tuning erlang build.

--- erlang.rb
+++ erlang.rb
@@ -1,6 +1,6 @@
 dep 'erlang.src' do
   requires 'libssl headers.managed', 'ncurses.managed'
   source 'git://github.com/erlang/otp.git'
-  preconfigure { shell 'git checkout OTP_R14B01' }
+  preconfigure { shell 'git checkout OTP_R14B01 && autoconf' }
   provides 'erl', 'erlc'
 end
