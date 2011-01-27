f4f5eeb1bc3a72d2b367a0748a33a3f69bd7cfb4
Mark Wolfe <mark@wolfe.id.au>
2011-01-27 22:06:18 +1100

    I think i got this shit now..

--- systembase.rb
+++ systembase.rb
@@ -1,3 +1,15 @@
 dep 'systembase' do
   requires 'vim.managed', 'curl.managed', 'htop.managed', 'jnettop.managed', 'screen.managed', 'nmap.managed', 'tree.managed'
 end
+dep 'htop.managed'
+dep 'java.managed' do
+  installs { via :apt, 'sun-java6-jre' }
+  after { shell "set -Ux JAVA_HOME /usr/lib/jvm/java-6-sun" }
+end
+dep 'jnettop.managed' do
+  installs { via :apt, 'jnettop' }
+end
+dep 'nmap.managed'
+dep 'screen.managed'
+dep 'tree.managed'
+dep 'vim.managed'
