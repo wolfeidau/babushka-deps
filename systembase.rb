4db9b47d990cea2e3cfc5d5d8d8ea64f46618642
Mark Wolfe <mark@wolfe.id.au>
2011-01-27 21:59:16 +1100

    Messing with my dep

--- systembase.rb
+++ systembase.rb
@@ -1,5 +1,3 @@
 dep 'systembase' do
-  requires {
-    on :linux, 'vim.managed', 'curl.managed', 'htop.managed', 'jnettop.managed', 'screen.managed', 'nmap.managed', 'tree.managed'
-  }
+  requires 'vim.managed', 'curl.managed', 'htop.managed', 'jnettop.managed', 'screen.managed', 'nmap.managed', 'tree.managed'
 end
