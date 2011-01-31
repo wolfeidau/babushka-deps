7883e122ff1f484aab5a96a36cdd8229aa08bd7e
Mark Wolfe <mark@wolfe.id.au>
2011-01-31 21:34:26 +1100

    Fixed riak dep again.. dumb clipboard

--- riak.rb
+++ riak.rb
@@ -1,4 +1,4 @@
-dep 'rabbitmq.src' do
+dep 'riak.src' do
     source 'http://downloads.basho.com/riak/riak-0.14/riak_0.14.0-1_i386.deb'
     process_source {
         sudo('dpkg -i riak_0.14.0-1_i386.deb')
