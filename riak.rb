6dcfda3a642ece6e6eef1162f9b6ef18505050f3
Mark Wolfe <mark@wolfe.id.au>
2011-02-02 13:08:51 +1100

    Added arch flag

--- riak.rb
+++ riak.rb
@@ -1,7 +1,10 @@
 dep 'riak.src' do
-    source 'http://downloads.basho.com/riak/riak-0.14/riak_0.14.0-1_i386.deb'
+    arch = `uname -p`.strip
+  
+    source "http://downloads.basho.com/riak/riak-0.14/riak_0.14.0-1_#{arch}.deb"
+    
     process_source {
-        sudo('dpkg -i riak_0.14.0-1_i386.deb')
+        sudo("dpkg -i riak_0.14.0-1_#{arch}.deb")
     }
     provides 'riak', 'riak-admin'
 end
