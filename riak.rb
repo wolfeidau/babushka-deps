8a0ce54bac736c2f33ab464cb93ed484be7e4bfc
Mark Wolfe <mark@wolfe.id.au>
2011-02-03 21:22:28 +1100

    working on arch setup for this deb

--- riak.rb
+++ riak.rb
@@ -1,6 +1,16 @@
 dep 'riak.src' do
     arch = `uname -m`.strip
   
+    if arch == "unknown" || arch =~ / /
+        arch = `uname -m`.strip
+    end
+
+    if arch =~ /^i.86$/
+        arch = "i386"
+    elsif arch == "x86_64"
+        arch = "amd64"
+    end 
+  
     source "http://downloads.basho.com/riak/riak-0.14/riak_0.14.0-1_#{arch}.deb"
     
     process_source {
