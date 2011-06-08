46f75a4e46d2fbbe47ac3538d5e8349d6face003
Mark Wolfe <markw@wolfe.id.au>
2011-06-08 22:15:26 +1000

    bumped versions

--- erlang.rb
+++ erlang.rb
@@ -1,5 +1,5 @@
 dep 'erlang.src' do
   requires 'libssl headers.managed', 'ncurses.managed'
-  source 'http://www3.erlang.org/download/otp_src_R14B01.tar.gz'
+  source 'http://www3.erlang.org/download/otp_src_R14B02.tar.gz'
   provides 'erl', 'erlc'
 end
