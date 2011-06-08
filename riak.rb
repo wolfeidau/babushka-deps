84e7080ce24517724f6e44b5101df1058e4c98d0
Mark Wolfe <mark@wolfe.id.au>
2011-06-08 22:15:26 +1000

    bumped versions

--- riak.rb
+++ riak.rb
@@ -11,10 +11,10 @@ dep 'riak.src' do
         arch = "amd64"
     end 
   
-    source "http://downloads.basho.com/riak/riak-0.14/riak_0.14.0-1_#{arch}.deb"
+    source "http://downloads.basho.com/riak/riak-0.14/riak_0.14.2-1_#{arch}.deb"
     
     process_source {
-        sudo("dpkg -i riak_0.14.0-1_#{arch}.deb")
+        sudo("dpkg -i riak_0.14.2-1_#{arch}.deb")
     }
     provides 'riak', 'riak-admin'
 end
